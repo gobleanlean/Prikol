f56a26f308cc3589c490ef611d2aa040b9c8f7dc
Максим Егоров <mrxblank171@gmail.com>
2026-09-17 15:08:51 +0300

    Добавьте файлы проекта.

---
+++ Егоров Лаба/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Егоров_Лаба
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private string Form1_Load(object sender, EventArgs e)
+        {
+            "brrrrrrrr";
+        }
+    }
+}
